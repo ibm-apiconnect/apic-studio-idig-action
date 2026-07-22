b64d4db21300a8790e541300fcc2f110ba61d495
Mantvydas Luksas <mantvydas.luksas@ibm.com>
2026-07-22 15:22:10 +0100

    Change field name to project for form data

--- src/idig-client.js
+++ src/idig-client.js
@@ -29,7 +29,10 @@ let publishProjects = async function(workspacePath, folders, idigHost, platformA
     console.log(`Zip created at: ${zipPath}`);
     const curlUrl = `https://${platformApiPrefix}.${idigHost}/idig-broker/publish`;
     const formData = new FormData();
-    formData.append('zipFile', fs.readFileSync(zipPath).toString('base64'));
+    formData.append('project', fs.createReadStream(zipPath), {
+        filename: outputFile,
+        contentType: 'application/zip'
+    });
     const resp = await createOrUpdateProjects(curlUrl, formData, 'POST');
     fs.unlink(zipPath, (err) => {
         if (err) throw err;
